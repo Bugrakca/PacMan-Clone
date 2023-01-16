ab6afeae53e5137b9a92e2aeb25c847d79e2a774
Buğra Akca <bgrakca@gmail.com>
2023-01-16 15:52:42 +0300

    Pacman Movement

---
+++ Assets/_Project/_Scripts/Pacman.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Movement))]
+public class Pacman : MonoBehaviour
+{
+    public Movement Movement { get; private set; }
+
+    private void Awake()
+    {
+        Movement = GetComponent<Movement>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            Movement.SetDirection(Vector2.up);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            Movement.SetDirection(Vector2.down);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            Movement.SetDirection(Vector2.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            Movement.SetDirection(Vector2.right);
+        }
+
+        var angle = Mathf.Atan2(Movement.Direction.y, Movement.Direction.x);
+        transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.forward);
+        
+    }
+}
