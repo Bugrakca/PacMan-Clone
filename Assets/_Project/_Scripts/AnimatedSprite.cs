9206c69a9832a12889eb8dcc092fbb9c3917108c
Buğra Akca <bgrakca@gmail.com>
2023-01-16 15:53:02 +0300

    Sprite Animation

---
+++ Assets/_Project/_Scripts/AnimatedSprite.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class AnimatedSprite : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer { get; private set; }
+    public Sprite[] sprites;
+    public float animationTime = 0.125f;
+    public int animationFrame { get; private set; }
+    public bool loop = true;
+    
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(Animate), animationTime, animationTime);
+    }
+
+    private void Animate()
+    {
+        if (!spriteRenderer.enabled)
+        {
+            return;
+        }
+        
+        animationFrame++;
+
+        if (animationFrame >= sprites.Length && loop)
+        {
+            animationFrame = 0;
+        }
+
+        if (animationFrame >= 0 && animationFrame < sprites.Length)
+        {
+            spriteRenderer.sprite = sprites[animationFrame];
+        }
+    }
+
+    public void Restart()
+    {
+        animationFrame = -1;
+        Animate();
+    }
+}
